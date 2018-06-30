078240a6939a9cf5fffe706ab82c59f45b7da1e0
Dfuck Choudhary <31347702+Dfuck@users.noreply.github.com>
2018-06-30 18:45:27 +0530

    New RSPS
    In this program , I fix paisa problem which is now showing accurately result.

---
+++ NEWRSPS.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int r;
+float m,p;
+clrscr();
+printf("Enter ur money");
+scanf("%f",&m);
+r=(m);
+p=(m-r)*100;
+printf("rs=%d\n",r);
+printf("ps=%.f",p);
+
+getch();
+}

c916666a8c5d68691372e47cfeb7100b481cf0e1
Deepak Choudhary <31347702+Hey-Deepak@users.noreply.github.com>
2019-09-18 20:19:43 +0530

    Comma Right Most Value

---
+++ CommaRightMost.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        :Hello world
+ Author      : Deepak Choudhary
+ Description : In this Program I observe that .
+ ============================================================================
+ */
+
+
+#include <stdio.h>
+int main() 
+{	
+	int x = (1, 2, 3, 4, 5);
+	printf("%d",x);
+	
+	return 0;
+}

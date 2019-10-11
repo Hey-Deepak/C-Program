c74f0439807d3783955d7c2db32aa11dc4b15874
hitesh garg <49817807+hiteshgarg1999@users.noreply.github.com>
2019-10-12 05:15:07 +0530

    Exe Deleted

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,10 +1,22 @@
-
 #include<stdio.h>
 
-void main()
-{
-    printf("");
-    scanf("");
+int main()
+
+{   
+    int x;
+    printf("Enter Your Age: \n");
+    scanf("%d", &x);
+
+    if (x>=18)
+    {
+       printf("Your age is %d and You are eligible to Cast Vote \nCongratulation. \n:D", x);
+    }
     
-    return 0;
+    else
+    {
+       printf("Your age is %d and You are Not eligible to Cast Vote \nAfter %d year you can cast your Vote.\nBetter Luck Next Time.\n:D", x, 18-x);
+    }
+    
+    
+   return 0;
 }

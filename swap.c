749d0dee276afaf27c042d43cb9bc93204886cda
Deepak Choudhary <gljat999@gmail.com>
2019-10-16 07:29:42 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    
+    printf("Enter the value of a and b:- \n", );
+    scanf("%d %d", &a, &b);
+    printf("Before Swapping \n");
+    printf("a value is %d \nb value is %d \n", a, b);;
+    return 0;  
+}

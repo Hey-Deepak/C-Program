61e130ab6c7d019e80350ef1ea0b0f028eb73349
Deepak Choudhary <gljat999@gmail.com>
2019-10-17 00:02:36 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,11 +1,24 @@
 #include<stdio.h>
+swap(int x, int y)
+{
+    int temp;
+    temp = x;
+    x = y;
+    y = x;
+    return 
+
+}
+
 int main()
 {
     int a,b;
     
-    printf("Enter the value of a and b:- \n", );
+    printf("Enter the value of a and b:- \n" );
     scanf("%d %d", &a, &b);
     printf("Before Swapping \n");
-    printf("a value is %d \nb value is %d \n", a, b);;
+    printf("a value is %d \nb value is %d \n", a, b);
+    swap(printf("a value is %d \nb value is %d \n", a, b);a, b);
+    printf("After Swapping \n");
+    printf("a value is %d \nb value is %d \n", a, b);
     return 0;  
 }

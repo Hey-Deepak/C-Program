eede155bec76992f1551ce55ce79ec56755d8c7d
Deepak Choudhary <gljat999@gmail.com>
2019-10-17 09:19:08 +0530

    Update GatsV2.c

--- GatsV2.c
+++ GatsV2.c
@@ -1,11 +1,11 @@
-#include "numbers.h"
 #include<stdio.h>
 int main()
-{   int x;
-    printf("Enter a number: ");
-    scanf("%d", &x);
-    printf("%d %s a Armstrong number", x, isArmstrongNo(x)==0 ? "is not" : "is");
-    
-    //printf("No. of digits in %d are %d", x, getNoOfDigits(x));
+{
+    char c;
+   
+    while ((c = getchar()) != EOF && c!= 10)
+    {
+        putchar(c);
+    }
     return 0;  
 }

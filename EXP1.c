35dfbc060d11d0e191d93799b5a9c1f79871ad55
Deepak Choudhary <gljat999@gmail.com>
2019-10-22 11:43:39 +0530

    Update EXP1.c

--- EXP1.c
+++ EXP1.c
@@ -3,5 +3,15 @@ int main()
 {
     
     printf("%d\n",sizeof(int)>-1);
+    if (sizeof(int)>-1)
+    {
+        printf("True \n");
+    }
+    else
+    {
+        printf("False \n");
+    }
+    
+    
     return 0;  
 }

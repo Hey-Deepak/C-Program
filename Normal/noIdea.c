acf3997dcba1e31cdf45cd13c0564ddd89fde43f
Deepak Choudhary <31347702+Hey-Deepak@users.noreply.github.com>
2020-03-18 23:59:16 +0530

    Delete noIdea.c

--- Normal/noIdea.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    printf("I have no Idea!\n");
-    printf("Why ???\n");
-    printf("I also have no idea why you wrote this program in the first place! ¯\_ಠ_ಠ_/¯\n");
-    return 0;
-
-}

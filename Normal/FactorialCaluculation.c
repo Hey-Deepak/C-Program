b295dfbfe5d5c7f00a8de9e8976bc8c7b62ee561
Deepak Choudhary <31347702+Hey-Deepak@users.noreply.github.com>
2020-03-18 23:58:39 +0530

    Create FactorialCaluculation.c

---
+++ Normal/FactorialCaluculation.c
@@ -0,0 +1,25 @@
+  
+// factorial caluculation
+
+#include <stdio.h>
+
+int factorial(int number)
+{
+    if (number == 1 || number == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return (number * factorial(number - 1));
+    }
+}
+int main()
+{
+    int num;
+    printf("enter the number:\n");
+    scanf("%d",&num);
+    printf("the factorial of %d is %d\n",num,factorial(num));
+    
+    return 0;
+}
